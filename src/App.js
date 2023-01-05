308ddd9fd79e16ec5b64be46bd3cfbd3191ebff1
Ashfak Uddin <ashmanpapet@gmail.com>
2023-01-04 23:56:57 -0500

    Month's opening/closing animations have the basis to them

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import MonthComponent from './monthcomponent.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <MonthComponent />
+    </div>
+  );
+}
+
+export default App;
