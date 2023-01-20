e32a433ba3641187482a916e3e48c96a556f68b4
Ashfak Uddin <ashmanpapet@gmail.com>
2023-01-20 00:17:36 -0500

    Added scrollign to day view

--- src/Components/dayview.js
+++ src/Components/dayview.js
@@ -2,12 +2,24 @@ import React from "react";
 import './dayview.css';
 
 const DayView = (props) => {
-    let items = [1,2,3,4,5];
+    let items = ["use the computed tab to see an Element's style rules, and there's a -> button for each rule to jump to where it's inherited from",2,3,4,5];
+    let hours = [];
+    for (let i = 1; i < 12; i++)
+    {
+        hours.push(<div>{i}am</div>)
+    }
+    hours.push(<div>Noon</div>);
+    for (let i = 1; i < 12; i++)
+    {
+        hours.push(<div>{i}pm</div>);
+    }
+    hours.push(<div>Midnight</div>);
     items = items.map(item => <li>{item}</li>);
     return <div id = "dayview">
         <div id = "left">Back</div>
         <div id = "middle">
-            <ul>
+            <div id = "time">{hours}</div>
+            <ul id = "list">
                 {items}
             </ul>
 
