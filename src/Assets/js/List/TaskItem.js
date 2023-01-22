f5c194724f35c79dfedf2ac8783ca811099fe4f5
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-01-22 00:20:50 -0500

    task files

---
+++ src/Assets/js/List/TaskItem.js
@@ -0,0 +1,60 @@
+export class TaskItem {
+    #parentList;
+    #id;
+    #title;
+    #description;
+    #due;
+    #done;
+
+    constructor(parentList, id, title, description, due, done) {
+        this.#parentList = parentList;
+        this.#id = id;
+        this.#title = title;
+        this.#description = description;
+        this.#due = due;
+        this.#done = done;
+    }
+
+    /**
+     * Deletes the task.
+     */
+    delete() {
+        this.#parentList.deleteItem(this.#id);
+    }
+
+    get title() {
+        return this.#title;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get due() {
+        return this.#due;
+    }
+
+    get done() {
+        return this.#done;
+    }
+
+    setTitle(newTitle) {
+        this.#parentList.setTitle(this.#id, newTitle);
+        this.#title = newTitle;
+    }
+
+    setDescription(newDescription) {
+        this.#parentList.setDescription(this.#id, newDescription);
+        this.#description = newDescription;
+    }
+
+    setDueDate(newDue) {
+        this.#parentList.setDueDate(this.#id, newDue);
+        this.#due = newDue;
+    }
+
+    setStatus(done) {
+        this.#parentList.setStatus(this.#id, done);
+        this.#done = done;
+    }
+}
---
+++ src/Assets/js/List/TaskList.js
@@ -0,0 +1,131 @@
+import { TaskItem } from "./TaskItem";
+
+/**
+ * Client for interacting with Google Tasks API.
+ */
+export class TaskList {
+    #listId;
+    #nextPageToken;
+
+    constructor() {
+        gapi.client.tasks.tasklists.list().then((response) => {
+            this.#listId = response.result.items[0].id;
+        });
+    }
+
+    /**
+     * Gets an array of tasks.
+     * @returns {Array<TaskItem>} An array of tasks.
+     */
+    async getItems() {
+        let tasks = [];
+
+        await gapi.client.tasks.tasks.list({
+            tasklist: this.#listId,
+            showCompleted: false,
+            showHidden: false,
+            maxResults: 100
+        }).then((response) => {
+            for (let task of response.result.items) {
+                tasks.push(new TaskItem(task.id, task.title, task.notes, Date(Date.parse(task.due))));
+            }
+        });
+
+        return tasks;
+    }
+
+    /**
+     * Gets a single task given its ID.
+     * @param {String} id The ID of the task to get.
+     * @returns {TaskItem} The task with the given ID.
+     */
+    async getItem(id) {
+        await gapi.client.tasks.tasks.get({
+            tasklist: this.#listId,
+            task: id
+        }).then((response) => {
+            return new TaskItem(response.result.id, response.result.title, response.result.notes, Date(Date.parse(response.result.due)));
+        });
+    }
+
+    /**
+     * Adds a task to the list.
+     * @param {String} name The name of the task.
+     * @param {String} description The description of the task.
+     * @param {Date} due The due date of the task.
+     * @returns {TaskItem} The task that was added.
+     */
+    async addItem(name, description, due) {
+        await gapi.client.tasks.tasks.insert({
+            tasklist: this.#listId,
+            title: name,
+            notes: description,
+            due: due.toISOString()
+        }).then((response) => {
+            return new TaskItem(response.result.id, response.result.title, response.result.notes, Date(Date.parse(response.result.due)));
+        });
+    }
+
+    /**
+     * Deletes a task from the list.
+     * @param {String} id The ID of the task to delete.
+     */
+    deleteItem(id) {
+        gapi.client.tasks.tasks.delete({
+            tasklist: this.#listId,
+            task: id
+        });
+    }
+
+    /**
+     * Sets the title of a task.
+     * @param {String} id The ID of the task to update.
+     * @param {String} newTitle The new title of the task.
+     */
+    setTitle(id, newTitle) {
+        gapi.client.tasks.tasks.patch({
+            tasklist: this.#listId,
+            task: id,
+            title: newTitle
+        });
+    }
+
+    /**
+     * Sets the description of a task.
+     * @param {String} id The ID of the task to update.
+     * @param {String} newDescription The new description of the task.
+     */
+    setDescription(id, newDescription) {
+        gapi.client.tasks.tasks.patch({
+            tasklist: this.#listId,
+            task: id,
+            notes: newDescription
+        });
+    }
+
+    /**
+     * Sets the due date of a task.
+     * @param {String} id The ID of the task to update.
+     * @param {Date} newTime The new due date of the task.
+     */
+    setDueDate(id, newTime) {
+        gapi.client.tasks.tasks.patch({
+            tasklist: this.#listId,
+            task: id,
+            due: newTime.toISOString()
+        });
+    }
+
+    /**
+     * Sets the status of a task.
+     * @param {String} id The ID of the task to update.
+     * @param {Boolean} done Whether the task is done or not.
+     */
+    setStatus(id, done) {
+        gapi.client.tasks.tasks.patch({
+            tasklist: this.#listId,
+            task: id,
+            status: done ? "completed" : "needsAction"
+        });
+    }
+}
