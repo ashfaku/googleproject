b8a8bddcd2db1080e55e0f938d002389cc0ed041
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-01-22 22:24:41 -0500

    planner driver

---
+++ src/Assets/js/Application/planner.js
@@ -0,0 +1,21 @@
+import { Calendar } from "../Calendar/Calendar";
+
+const calendarClient = new Calendar();
+let todayEvents;
+
+export function initializePlanner(year, month, day) {
+    todayEvents = calendarClient.getItems(year, month, day);
+    display();
+}
+
+export function display() {
+    // display today's date
+    // set where <-- and --> buttons go
+
+    for (let event of todayEvents) {
+        // plot the event on the scrolling time graph
+        // based on event.start and event.end
+        // if typeof event === WorkTimeBlock, on click pass event to the worktimeblock details page
+        // else on click pass event to the event details page
+    }
+}
