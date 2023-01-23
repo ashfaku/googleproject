6041959307054144a01eeeac6e857df014be3f9a
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-01-23 15:02:38 -0500

    import CalendarItem

--- src/Assets/js/Application/eventDetailPage.js
+++ src/Assets/js/Application/eventDetailPage.js
@@ -1,5 +1,10 @@
+import { CalendarItem } from "../Calendar/CalendarItem";
+
 let event;
 
+/**
+ * @param {CalendarItem} event The CalendarItem to display details for.
+ */
 export function initializeEventDetailPage(event) {
     this.event = event;
     display();
