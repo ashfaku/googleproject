b4ee6030f6cab36c6b75b654d8e8cae5892b73a2
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-01-23 16:37:01 -0500

    fix sign in handling

--- src/Components/dayview.js
+++ src/Components/dayview.js
@@ -1,35 +1,34 @@
 import { React, useEffect } from "react";
 import './dayview.css';
-import {initializePlanner} from "../Assets/js/Application/planner";
 
 const DayView = (props) => {
-    let items = ["use the computed tab to see an Element's style rules, and there's a -> button for each rule to jump to where it's inherited from",2,3,4,5];
+    let items = ["use the computed tab to see an Element's style rules, and there's a -> button for each rule to jump to where it's inherited from", 2, 3, 4, 5];
     let hours = [];
     useEffect(() => {
         // Code that should run after the component is mounted
-      //  console.log(initializePlanner(props.year, props.month, props.day));
-      }, []);
-    for (let i = 1; i < 12; i++)
-    {
+        import("../Assets/js/Application/planner").then(plannerDriver => {
+            console.log(plannerDriver.initializePlanner(props.year, props.month, props.day));
+        });
+    }, []);
+    for (let i = 1; i < 12; i++) {
         hours.push(<div>{i}am</div>)
     }
     hours.push(<div>Noon</div>);
-    for (let i = 1; i < 12; i++)
-    {
+    for (let i = 1; i < 12; i++) {
         hours.push(<div>{i}pm</div>);
     }
     hours.push(<div>Midnight</div>);
     items = items.map(item => <li>{item}</li>);
-    return <div id = "dayview">
-        <div id = "left">Back</div>
-        <div id = "middle">
-            <div id = "time">{hours}</div>
-            <ul id = "list">
+    return <div id="dayview">
+        <div id="left">Back</div>
+        <div id="middle">
+            <div id="time">{hours}</div>
+            <ul id="list">
                 {items}
             </ul>
 
         </div>
-        <div id = "right">Forward</div>
+        <div id="right">Forward</div>
 
     </div>
 
--- src/Components/login.js
+++ src/Components/login.js
@@ -1,14 +1,15 @@
-import {React, useEffect } from "react";
+import { React, useEffect } from "react";
 import MonthComponent from "../Components/monthcomponent";
 const Login = (props) => {
-    const button = <button onClick = {() => {
-        
-        window.signIn();
-        props.root.render(<div>
-            <MonthComponent root = {props.root} />
+    const button = <button onClick={() => {
 
-        </div>)
-    }} id = "loginB">Sign in With Google</button>;
+        window.signIn(() => {
+            props.root.render(<div>
+                <MonthComponent root={props.root} />
+
+            </div>)
+        });
+    }} id="loginB">Sign in With Google</button>;
     return <div>
         {button}
     </div>;
