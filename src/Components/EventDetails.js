d3425887829e437a9b68db24b0d6c11605c3dc60
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-02-15 14:31:56 -0500

    basic event details

---
+++ src/Components/EventDetails.js
@@ -0,0 +1,25 @@
+import { React, useEffect } from "react";
+import '../Assets/Styles/eventDetails.css';
+
+const EventDetails = (props) => {
+
+    useEffect(() => {
+        // Code that should run after the component is mounted
+        import("../Assets/js/Application/eventDetailPage").then(eventDetailsDriver => {
+            eventDetailsDriver.initializeEventDetailPage(props.event);
+        });
+    }, []);
+
+    return <div id="event-details">
+        <h1 id="title">Event Title</h1>
+        <p id="description">Event description</p>
+        <br />
+        From <span id="start-time">Start time</span>
+        <br />
+        To <span id="end-time">End time</span>
+    </div>
+
+};
+
+
+export default EventDetails;
