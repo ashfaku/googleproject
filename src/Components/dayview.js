44da1be838393484fecf6917e2f9c2be85253900
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-04-09 22:30:59 -0400

    data for backlinking and button to add timeblocks

--- src/Components/dayview.js
+++ src/Components/dayview.js
@@ -4,6 +4,7 @@ import { monthNameToNumber } from "../Assets/js/utilities/utils";
 import '../Assets/Styles/dayview.css';
 import EventDetails from "./EventDetails";
 import TaskDetails from "./TaskDetails";
+import TaskList from "./TaskList";
 
 const DayView = (props) => {
     let hours = [];
@@ -22,13 +23,13 @@ const DayView = (props) => {
                     if (typeof item === WorkTimeBlock) {
                         elements.push(<li><button onClick={() => {
                             props.root.render(<div>
-                                <TaskDetails root={props.root} event={item} />
+                                <TaskDetails root={props.root} event={item} backYear={props.year} backMonth={props.month} backDay={props.day} />
                             </div>);
                         }}>{item.title}</button></li >);
                     } else {
                         elements.push(<li><button onClick={() => {
                             props.root.render(<div>
-                                <EventDetails root={props.root} event={item} />
+                                <EventDetails root={props.root} event={item} backYear={props.year} backMonth={props.month} backDay={props.day} />
                             </div>);
                         }}>{item.title}</button></li>);
                     }
@@ -57,7 +58,11 @@ const DayView = (props) => {
 
         </div>
         <div id="right">Forward</div>
-
+        <button id="add-block-button" onClick={() => {
+            props.root.render(<div>
+                <TaskList root={props.root} backYear={props.year} backMonth={props.month} backDay={props.day}></TaskList>
+            </div>);
+        }}>➕</button>
     </div>
 
 };
