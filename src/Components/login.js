aa4fee4995555bfb52915d4bdbfd94c66daeab0b
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2023-05-01 01:55:55 -0400

    login text

--- src/Components/login.js
+++ src/Components/login.js
@@ -10,7 +10,25 @@ const Login = (props) => {
             </div>)
         });
     }} id="loginB">Sign in With Google</button>;
-    return <div>
+    return <div>Welcome to the Planner App
+        <br /><br />
+        The main screen will show you
+        your events and tasks for the day.
+        <br /><br />
+        You can press the + button
+        to dedicate a block of time for
+        a certain task.
+        <br /><br />
+        Clicking on an event or task
+        will give you details about it.
+        <br /><br />
+        This planner uses your Google
+        Calendar and Tasks to identify
+        your events and tasks.
+        Your data never leaves your
+        neither your browser nor your
+        Google account.
+        <br /><br />
         {button}
     </div>;
 };
